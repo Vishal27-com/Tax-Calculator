4e038925d4313d8db20ff0db65570bf141c76120
Vishal27-com <vishalgupta962148@gmail.com>
2023-06-20 12:06:18 +0530

    initial commit

---
+++ src/Pages/AdvTax.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdvTax = () => {
+  return (
+    <div>AdvTax</div>
+  )
+}
+
+export default AdvTax
---
+++ src/Pages/HRA.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HRA = () => {
+  return (
+    <div>HRA</div>
+  )
+}
+
+export default HRA
