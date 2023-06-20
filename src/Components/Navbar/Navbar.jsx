11f97fe76b821d8dfe8884bdd91aa715f42c2db8
Vishal27-com <vishalgupta962148@gmail.com>
2023-06-20 12:38:07 +0530

    navbar made

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import './navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav-cont'>
+      <h3>Tax Calculator</h3>
+      <div className='link-box'>
+      <h4>HRA</h4>
+      <h4>Advance Tax</h4>  
+      </div>
+    </div>
   )
 }
 
