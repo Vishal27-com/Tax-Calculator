42d9e95a154454b087ae0e19429f4c750a08d44f
Vishal27-com <vishalgupta962148@gmail.com>
2023-06-20 13:14:25 +0530

    navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
+import { Link } from 'react-router-dom'
+import {MdMenu} from "react-icons/md"
 const Navbar = () => {
+  const [show,setShow]=useState(false)
+  const toggleHandler=()=>{
+   setShow(!show)
+  }
   return (
+    <>
     <div className='nav-cont'>
       <h3>Tax Calculator</h3>
       <div className='link-box'>
+        <Link to='/'>
       <h4>HRA</h4>
+        </Link>
+        <Link to='/adv-tax'>
       <h4>Advance Tax</h4>  
+        </Link>
+      </div>
+      {/* For small screens */}
+      <div className='mob-nav'>
+      <MdMenu size={30} onClick={toggleHandler} />
+      {
+        show && 
+        <div className='mob-link'>
+          <Link to='/'>
+      <h4>HRA</h4>
+        </Link>
+        <Link to='/adv-tax'>
+      <h4>Advance Tax</h4>  
+        </Link> 
+        </div>
+      }
       </div>
     </div>
+    
+      </>
   )
 }
 
