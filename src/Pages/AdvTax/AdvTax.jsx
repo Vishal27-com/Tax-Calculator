4ef08b74b9a54d872ed6dded846a06bcbee75a11
Vishal27-com <vishalgupta962148@gmail.com>
2023-06-21 10:52:09 +0530

    advtax

---
+++ src/Pages/AdvTax/AdvTax.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../hra.css";
+import AdvTaxForm from "../../Components/Form/AdvTaxForm/AdvTaxForm";
+const AdvTax = () => {
+  return (
+    <div className="hra-div">
+      <h2 className="title">Advance Tax</h2>
+      <AdvTaxForm />
+    </div>
+  );
+};
+
+export default AdvTax;
