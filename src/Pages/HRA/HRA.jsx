dcb02768179a08cbccd638708f9100725969c949
Vishal27-com <vishalgupta962148@gmail.com>
2023-06-21 10:52:37 +0530

    hra updated

---
+++ src/Pages/HRA/HRA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../hra.css'
+import HRAForm from '../../Components/Form/HRAForm/HRAForm'
+const HRA = () => {
+  return (
+    <div className='hra-div'>
+      <h2 className='title'>House Rent Allowance</h2>
+      <HRAForm />
+    </div>
+  )
+}
+
+export default HRA
